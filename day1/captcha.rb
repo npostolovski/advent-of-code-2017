ed16f1706ec20a7ae7ebb0007d9f0a4823f5dac6
Tash Postolovski <npostolovski@gmail.com>
2017-12-01 18:53:37 +1100

    Solve 1122 example

--- day1/captcha.rb
+++ day1/captcha.rb
@@ -1,7 +1,24 @@
 class Captcha
   attr_reader :array
-  
+
   def initialize(array)
     @array = array
+  end
+
+  def answer
+    numbers_with_matches = []
+
+    array.each_with_index do |digit, i|
+      # if i+1 == array.length
+      #   if array[i] == array[0]
+      #     numbers_with_matches << array[i]
+      #     return numbers_with_matches
+      #   end
+      # end
+      if array[i] == array[i+1]
+        numbers_with_matches << array[i]
+      end
+    end
+    numbers_with_matches.inject(0) { |sum, memo| sum + memo }
   end
 end
--- day1/spec/captcha_spec.rb
+++ day1/spec/captcha_spec.rb
@@ -8,4 +8,12 @@ describe Captcha do
 
     expect(captcha.array).to eq array
   end
+
+  it "1122 should produce a sum of 3" do
+    array1 = [1, 1, 2, 2]
+    expected_answer = 3
+    captcha = Captcha.new(array1)
+
+    expect(captcha.answer).to eq expected_answer
+  end
 end
