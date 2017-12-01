2e902f8f1661d55bc1e5e10cf23c9ab0a3150493
Tash Postolovski <npostolovski@gmail.com>
2017-12-01 19:23:55 +1100

    check that method returns false if it is not the last element in the array

--- day1/spec/unit/digit_matcher_spec.rb
+++ day1/spec/unit/digit_matcher_spec.rb
@@ -2,13 +2,20 @@ require_relative '../../lib/digit_matcher'
 
 describe DigitMatcher do
   context '#it_is_the_last_digit_in_array' do
-    it 'is true when the index is the last element' do
-      array = [1, 2, 1]
-      i = 2
+    let(:array) { [1, 2, 1] }
 
-      digit_matcher = DigitMatcher.new(array, i)
+    it 'is true when the index is the last element' do
+      index_of_last_element = 2
+      digit_matcher = DigitMatcher.new(array, index_of_last_element)
 
       expect(digit_matcher.it_is_the_last_digit_in_array).to eq true
+    end
+
+    it 'is false when the index is not the last element' do
+      index_of_first_element = 0
+      digit_matcher = DigitMatcher.new(array, index_of_first_element)
+
+      expect(digit_matcher.it_is_the_last_digit_in_array).to eq false
     end
   end
 end
