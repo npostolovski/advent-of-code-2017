e661d66c08e100af98d54dc295b1327bcbdeb944
Tash Postolovski <npostolovski@gmail.com>
2017-12-01 19:14:05 +1100

    refactoring to make the algorithm easier to understand

--- day1/lib/captcha.rb
+++ day1/lib/captcha.rb
@@ -9,20 +9,32 @@ class Captcha
     numbers_with_matches = []
 
     array.each_with_index do |digit, i|
-      if i+1 == array.length
-        if array[i] == array[0]
+      if it_is_the_last_digit_in_array(i, array)
+        if last_digit_matches_first(i, array)
           numbers_with_matches << array[i]
         end
-      elsif array[i] == array[i+1]
+      elsif digit_matches_next(i, array)
         numbers_with_matches << array[i]
       end
     end
-    sum(numbers_with_matches)
+    sum_of(numbers_with_matches)
   end
 
   private
 
-  def sum(numbers_with_matches)
+  def sum_of(numbers_with_matches)
     numbers_with_matches.inject(0) { |sum, memo| sum + memo }
+  end
+
+  def it_is_the_last_digit_in_array(i, array)
+    i + 1 == array.length
+  end
+
+  def last_digit_matches_first(i, array)
+    array[i] == array[0]
+  end
+
+  def digit_matches_next(i, array)
+    array[i] == array[i+1]
   end
 end
